a460cd47b0f6f8a4b94355df567ac0d8afd0ef34
カトパン <katobad.0405@gmail.com>
2022-08-31 11:25:21 +0900

    ビルド修正

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,11 +1,12 @@
 import { client } from "../../libs/client";
 import styles from '../../styles/Home.module.scss';
+import type { Blog } from '../../types/blog'
 
-export default function BlogId({ blog }) {
+export default function BlogId({ blog }: { blog: Blog }) {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{blog.title}</h1>
-      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <p className={styles.publishedAt}>{blog.category}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.content}`,
@@ -20,12 +21,12 @@ export default function BlogId({ blog }) {
 export const getStaticPaths = async () => {
   const data = await client.get({ endpoint: "blogs" });
 
-  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  const paths = data.contents.map((content: any) => `/blog/${content.id}`);
   return { paths, fallback: false };
 };
 
 // データをテンプレートに受け渡す部分の処理を記述
-export const getStaticProps = async (context) => {
+export const getStaticProps = async (context: any) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: "blogs", contentId: id });
 console.log(data);
